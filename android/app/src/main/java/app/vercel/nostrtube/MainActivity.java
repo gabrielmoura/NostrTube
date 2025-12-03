327c67a1b03f6bb7957a691918f6e45c02526d82
Gabriel Moura <gmouradev96@gmail.com>
2025-12-03 12:08:36 -0300

    Implementa Capacitor para compatibilidade com Android e IOS

---
+++ android/app/src/main/java/app/vercel/nostrtube/MainActivity.java
@@ -0,0 +1,5 @@
+package app.vercel.nostrtube;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.vercel.nostrtube',
+  appName: 'NostrTube',
+  webDir: 'dist',
+};
+
+export default config;
